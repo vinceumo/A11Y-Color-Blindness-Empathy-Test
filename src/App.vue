3b3688b1fa874ce07cad0f5317982cd5d4871d30
Vincent Humeau <hello@vincent-humeau.com>
2018-08-10 15:19:02 +0100

    Set up scss

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $primary;
   margin-top: 60px;
 }
 </style>
