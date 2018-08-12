f2b01fac44fc0581e53c522272efa24feb1361df
Vincent Humeau <hello@vincent-humeau.com>
2018-08-12 19:38:15 +0100

    Started search

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Search/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Search
   }
 }
 </script>
---
+++ src/components/Search.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Hello</p>
+</template>
+
+<script>
+export default {
+  name: 'Search'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
