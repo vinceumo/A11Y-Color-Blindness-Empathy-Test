32958bc464b6a23e706c73fe2eb55f27f47cdc9a
Vincent Humeau <hello@vincent-humeau.com>
2018-08-21 17:33:53 +0100

    Started the checklist

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <div class="colorblind8">
+    <div>
+      <Checklist v-for="(item, index) in tests" v-bind:key="item.name + index" v-bind:tests="{name: item.name, status: item.status}"/>
+    </div>
+    
+    <div v-bind:class="currentTest">
       <TestOutput v-if="showTestOutput" v-bind:pageToTest="pageToTestUrl"/>
     </div>
     <div>
@@ -11,17 +15,31 @@
 </template>
 
 <script>
+import Checklist from './components/Checklist.vue'
 import TestOutput from './components/TestOutput.vue'
 
 export default {
   name: 'app',
   components: {
+    Checklist,
     TestOutput
   },
   data: function () {
     return {
+      currentTest: "",
       pageToTestUrl: "https://www.cosmos.co.uk/",
-      showTestOutput: true
+      showTestOutput: true,
+      tests: [
+        {name: "protanopia", status: null},
+        {name: "protanomaly", status: null},
+        {name: "deuteranopia", status: null},
+        {name: "deuteranomaly", status: null},
+        {name: "tritanopia", status: null},
+        {name: "tritanomaly", status: null},
+        {name: "achromatopsia", status: null},
+        {name: "achromatomaly", status: null}, 
+        {name: "grayscale", status: null} 
+      ]
     }
   }
 }
---
+++ src/components/Checklist.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <label><input name="tests" type="radio"> {{tests.name}}</label>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Checklist',
+    props: ['tests'],
+  }
+</script>
