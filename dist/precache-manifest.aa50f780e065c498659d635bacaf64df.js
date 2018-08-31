b3cb742975db2f5118c698a1cb5179102f5dd493
Vincent Humeau <hello@vincent-humeau.com>
2018-08-31 18:41:08 +0100

    Added dist

---
+++ dist/precache-manifest.aa50f780e065c498659d635bacaf64df.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f41a56aa8f4f2fa98cbf",
+    "url": "/js/chunk-vendors.348ad41a.js"
+  },
+  {
+    "revision": "736de88955ce9d69e062",
+    "url": "/js/app.0c1e0a7a.js"
+  },
+  {
+    "revision": "1f20bb6585101d2cf95b8edc42d81be7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7875232ca9ba07e7e9791ea17b70d800",
+    "url": "/img/icon.svg"
+  },
+  {
+    "revision": "055fc1183ce34cf6180e2a33f2ea27a4",
+    "url": "/img/icon-1000x.png"
+  },
+  {
+    "revision": "e32eb7f4bb4eb04f94ad386c0e669996",
+    "url": "/img/accecss-filters.e32eb7f4.svg"
+  },
+  {
+    "revision": "736de88955ce9d69e062",
+    "url": "/css/app.f7cc411b.css"
+  },
+  {
+    "revision": "978bbda74525cec56414d46cdb4c0584",
+    "url": "/browserconfig.xml"
+  }
+];
---
+++ dist/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.aa50f780e065c498659d635bacaf64df.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "a11y-color-blindness-empathy-test"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
